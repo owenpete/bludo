c09909789d5eb397b85707fbc1d2879dbbe5a721
ogpea <ogpete23@gmail.com>
2021-06-14 14:09:33 -0500

    Fixed: bug where newly created database could not be found on first load

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -11,18 +11,16 @@ export default function list(req: any, res: any){
             complete: []
           }
         ));
-        res.status(200).json('success');
       }catch(err){
         res.status(500).json({err: err});
       }
-    }else{
-      try{
-        const response = fs.readFileSync(`${path}list.json`);
-        res.status(200).json(response);
-      }
-      catch(err: any){
-          res.status(200).json({err: 'read error '+err});
-      }
+    }
+    try{
+      const response = fs.readFileSync(`${path}list.json`);
+      res.status(200).json(response);
+    }
+    catch(err: any){
+        res.status(200).json({err: 'read error '+err});
     }
   }catch(err: any){
     res.status(500).json({err: err});
